c21c4d022f5028dfe6bf530eb2925fb82a1cbd80
sosuisen <hidekazu.kubota@gmail.com>
2024-04-27 21:42:16 +0900

    first commit

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,18 +1,13 @@
 import { Title } from "solid-start";
-import Counter from "~/components/Counter";
 
 export default function Home() {
   return (
     <main>
-      <Title>Hello World</Title>
-      <h1>Hello world!</h1>
-      <Counter />
+      <Title>storybook.jp</Title>
       <p>
-        Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
-        </a>{" "}
-        to learn how to build SolidStart apps.
+        storybook.jp (1996-2024)<br/>
+        <br/>
+        おやすみなさい。
       </p>
     </main>
   );
